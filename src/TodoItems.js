b1acd331bcc83ff13f73819161939362b5efb91b
Regina Chen <regina.y.chen@utexas.edu>
2018-03-04 20:17:39 -0600

    added animations

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import FlipMove from 'react-flip-move';
  
 class TodoItems extends Component {
 	constructor(props, context) {
@@ -32,7 +33,9 @@ class TodoItems extends Component {
  
     return (
       <ul className="todo-list">
+        <FlipMove duration={250} easing="ease-out">
           {listItems}
+        </FlipMove>
       </ul>
     );
   }
